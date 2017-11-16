d29cc53922c7ba598ad294a2903e3c27186e0f4d
SIMRAN1 <simranpreetkaurkhalsa13@gmail.com>
2017-11-16 18:40:12 +0530

    changing label on clicking button

--- mealtracker/ViewController.swift
+++ mealtracker/ViewController.swift
@@ -9,7 +9,10 @@
 import UIKit
 
 class ViewController: UIViewController {
-
+    //MARK:Pr
+    
+    @IBOutlet weak var mealNameLabel: UILabel!
+    @IBOutlet weak var nameTextField: UITextField!
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view, typically from a nib.
@@ -18,6 +21,9 @@ class ViewController: UIViewController {
     override func didReceiveMemoryWarning() {
         super.didReceiveMemoryWarning()
         // Dispose of any resources that can be recreated.
+    }
+    @IBAction func setDefaultLabelText(_ sender: Any) {
+        mealNameLabel.text="Default Text"
     }
 
 
