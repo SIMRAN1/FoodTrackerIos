ea3b632fa96acab0426f4face62358e3a75bfd31
SIMRAN1 <simranpreetkaurkhalsa13@gmail.com>
2017-11-29 16:30:36 +0530

    Table cell added

---
+++ mealtracker/MealTableViewCell.swift
@@ -0,0 +1,24 @@
+//
+//  MealTableViewCell.swift
+//  mealtracker
+//
+//  Created by SIMRANPREET KAUR on 28/11/17.
+//  Copyright © 2017 SIMRANPREET KAUR. All rights reserved.
+//
+
+import UIKit
+
+class MealTableViewCell: UITableViewCell {
+
+    override func awakeFromNib() {
+        super.awakeFromNib()
+        // Initialization code
+    }
+
+    override func setSelected(_ selected: Bool, animated: Bool) {
+        super.setSelected(selected, animated: animated)
+
+        // Configure the view for the selected state
+    }
+
+}
